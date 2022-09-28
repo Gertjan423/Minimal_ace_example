28c5a4fe9b2b0d3d0245a22e2793fd7f560b4f31
Gert-Jan Bottu <gertjanbottu@hotmail.com>
2022-09-28 17:27:26 +0200

    initial editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as ace from "ace-builds";
+import 'ace-builds/src-noconflict/mode-python';
+import 'ace-builds/src-noconflict/theme-chrome';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'minimal';
+  text = 'some text and some more text';
+
+  @ViewChild('codeboxId')
+  private codebox!: ElementRef<HTMLElement>;
+
+  ngAfterViewInit(): void {
+    // initialize ace
+    var langTools = ace.require("ace/ext/language_tools");
+
+    // initialize code editor
+    const codeEditor = ace.edit(this.codebox.nativeElement);
+    codeEditor.session.setMode("ace/mode/python");
+    codeEditor.setTheme("ace/theme/chrome");
+    codeEditor.setValue(this.text);
+    codeEditor.selection.clearSelection();
+  }
 }
